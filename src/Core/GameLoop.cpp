1cde6a4972f388520e72ee7b6cbe90621d954947
James <jameskr97@gmail.com>
2020-01-19 06:06:47 -0500

    Add FPS counter to GameLoop.

--- src/Core/GameLoop.cpp
+++ src/Core/GameLoop.cpp
@@ -1,7 +1,7 @@
+#include <iostream>
 #include "GameLoop.hpp"
 
 using namespace ETT::Core;
-
 
 void GameLoop::run() {
     this->init();
@@ -16,6 +16,7 @@ void GameLoop::run() {
         auto frameTime = newTime - currentTime; // Get difference (delta) of frame. (how long the frame took).
         currentTime = newTime; // Now that we have delta, change the starting time to the newTime.
         updateTime += frameTime; // Add (how long the frame took) to (the amount of time we have to update before we need to render again).
+        fpsTime += frameTime; // Add frameTime to fpsTime so we can track FPS once it is above 1second
 
         // As long as we have the time (in timestep [aka updates per second) increments, keep updating.
         // This allows us to update at a constant timestep, which letting the render function run as fast as it would like to.
@@ -24,7 +25,20 @@ void GameLoop::run() {
             updateTime -= timestep; // decrement by timestep
         }
 
+        // FPS Update Loop. After every second...
+        if(fpsTime.count() >= clock::period::den){
+            fpsTime = std::chrono::duration<double, std::nano>::zero(); // Reset fpsTime...
+            fps = frameCount; // Transfer frameCount to publicly accessible FPS variable.
+            frameCount = 0; // reset frameCount
+//            printf("%f\n", fps);
+        }
+
+        frameCount++;
         render();
     }
     this->shutdown();
+}
+
+double GameLoop::getFPS() {
+    return fps;
 }
--- src/Core/GameLoop.hpp
+++ src/Core/GameLoop.hpp
@@ -5,23 +5,29 @@
 
 /**
  * Articles used to help write this game loop
- *  - https://gafferongames.com/post/fix_your_timestep/
- *  - https://dewitters.com/dewitters-gameloop/
- *  - http://www.fabiensanglard.net/timer_and_framerate/index.php
+ *  1. https://gafferongames.com/post/fix_your_timestep/
+ *  2. https://dewitters.com/dewitters-gameloop/
+ *  3. http://www.fabiensanglard.net/timer_and_framerate/index.php
  */
+
 
 namespace ETT::Core {
 
 class GameLoop {
 public:
     void run();
+    double getFPS();
 
 private:
     // Constants
-    std::chrono::milliseconds timestep{16}; // FPS CONSTANT
+    std::chrono::milliseconds timestep{16}; // UPDATES PER SECOND CONSTANT
 
     // Variables
-    std::chrono::duration<double, std::nano> updateTime{0};
+    std::chrono::duration<double, std::nano> updateTime{0}; // updateTime accumulation (view link #1 above)
+
+    // FPS Tracking
+    double frameCount{0}, fps{0};
+    std::chrono::duration<double, std::nano> fpsTime{0};
 
     bool isRunning{false};
 
